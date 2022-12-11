72a49e2a7d6b406174dd06a4ee342d6470923c5c
Oleh <olegsimfi@gmail.com>
2022-12-11 14:23:07 +0200

    Add the Main page sections test

--- src/pages/main.page.ts
+++ src/pages/main.page.ts
@@ -1,15 +1,31 @@
-import { Locator } from '@playwright/test';
+import {Locator} from '@playwright/test';
 
 import config from '../../playwright.config';
-import { Timeout } from '../constants/timeouts';
+import {Timeout} from '../constants/timeouts';
 
-import { BasePage } from './base.page';
+import {BasePage} from './base.page';
 
 export class MainPage extends BasePage {
-     title: Locator = this.idLocator('nav-title');
+    title: Locator = this.idLocator('nav-title');
+    contactUs: Locator = this.idLocator('contact-us');
+    loginPortal: Locator = this.idLocator('login-portal');
+    buttonClicks: Locator = this.idLocator('button-clicks');
+    pageObjectModel: Locator = this.idLocator('page-object-model').nth(0);
+    pageObjectAccordion: Locator = this.idLocator('page-object-model').nth(1);
+    dropdownCheckboxesRadiobuttons: Locator = this.idLocator('dropdown-checkboxes-radiobuttons');
+    ajaxLoader: Locator = this.idLocator('ajax-loader');
+    actions: Locator = this.idLocator('actions');
+    scrollingAround: Locator = this.idLocator('scrolling-around');
+    popupAlerts: Locator = this.idLocator('popup-alerts');
+    iframe: Locator = this.idLocator('iframe');
+    hiddenElements: Locator = this.idLocator('hidden-elements');
+    dataTable: Locator = this.idLocator('data-table');
+    autocompleteTextfield: Locator = this.idLocator('autocomplete-textfield');
+    fileUpload: Locator = this.idLocator('file-upload');
+    datepicker: Locator = this.idLocator('datepicker');
 
     async openHomePage(): Promise<void> {
-        await this.page.goto(config.use.baseURL, { timeout: Timeout.L });
+        await this.page.goto(config.use.baseURL, {timeout: Timeout.L});
     }
 
     async waitForMainPageTitle(): Promise<void> {
--- src/tests/main-page.spec.ts
+++ src/tests/main-page.spec.ts
@@ -1,16 +1,42 @@
-import { expect } from '@playwright/test';
+import {expect} from '@playwright/test';
 
-import { test } from '../fixtures/page.fixtures';
+import {test} from '../fixtures/page.fixtures';
 import {mainPageTitle} from "../data/main-page";
 
 test.describe('Main page functionality', () => {
-    test.beforeEach(async ({ mainPage }) => {
+    test.beforeEach(async ({mainPage}) => {
         await mainPage.openHomePage();
         await mainPage.waitForMainPageTitle();
     });
 
-    test('should verify the Main page title', async ({ mainPage }) => {
+    test('should verify the Main page title', async ({mainPage}) => {
 
         expect(await mainPage.getText(mainPage.title)).toBe(mainPageTitle);
+    });
+
+    test('should verify Main page sections', async ({mainPage}) => {
+        mainPage.each(
+            [
+                mainPage.contactUs,
+                mainPage.loginPortal,
+                mainPage.buttonClicks,
+                mainPage.pageObjectModel,
+                mainPage.pageObjectAccordion,
+                mainPage.dropdownCheckboxesRadiobuttons,
+                mainPage.ajaxLoader,
+                mainPage.actions,
+                mainPage.scrollingAround,
+                mainPage.popupAlerts,
+                mainPage.iframe,
+                mainPage.hiddenElements,
+                mainPage.dataTable,
+                mainPage.autocompleteTextfield,
+                mainPage.fileUpload,
+                mainPage.datepicker
+            ],
+            async mainPageSections => {
+                expect(await mainPageSections.isVisible()).toBe(true);
+            }
+        );
     });
 });
